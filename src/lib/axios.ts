e1bee536d531eb46cdef9809bae7bec9793050cc
Andrew Vidiborets <a.vidiborets@gmsil.com>
2026-07-03 12:32:42 +0300

    init

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "/api";
+
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL,
   withCredentials: true,
 });
 
